fae02eff70b1b57c71956dd81f995b90fd817ba5
Armando Ferreira <armando.ferreira@edu.uag.mx>
2019-10-16 14:52:44 -0700

    Adding Shell Script for install VSCode Extensions

---
+++ vscode_install_extensions.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Shell Script for install VSCode Extensions
+
+code --install-extension adamhartford.vscode-base64
+code --install-extension bibhasdn.unique-lines
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension iansan5653.format-python-docstrings
+code --install-extension mrcrowl.hg
+code --install-extension ms-python.python
+code --install-extension ms-vscode.cpptools
+code --install-extension ms-vscode.vscode-typescript-tslint-plugin
+code --install-extension NathanRidley.autotrim
+code --install-extension njpwerner.autodocstring
+code --install-extension ryu1kn.partial-diff
+code --install-extension Tyriar.sort-lines
+code --install-extension wmaurer.change-case
+code --install-extension yo1dog.cursor-align
+
